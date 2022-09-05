b73e1bcffe06dd75fb64bbce0ec89c8c528eab28
paul-brisson <brissonpauljr@gmail.com>
2022-09-05 19:12:38 +0200

    Create bubbleSort.py

---
+++ algos/bubbleSort.py
@@ -0,0 +1,14 @@
+def bubbleSort(ar):
+    s = len(ar)
+    for i in range(s):
+        done = True
+        for j in range(s - i - 1):
+            if ar[j] > ar[j+1]:
+                ar[j],ar[j+1]=ar[j+1],ar[j]
+                done = False
+        if done == True:
+            break
+
+ar =[100,-2,50,-6,67,-80]
+bubbleSort(ar)
+print(ar)
