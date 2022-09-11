affd7fe5e0390c2a8e33dd3d688a3e75440fbf4a
paul-brisson <brissonpauljr@gmail.com>
2022-09-11 20:22:26 +0200

    Update timSort.py
    Still has bugs

--- algos/timSort.py
+++ algos/timSort.py
@@ -3,7 +3,10 @@ split into 'runs' of almost sorted
 chunks
 insertion_sort the chunks
 the merge_sort the chunks
+
+Still has bugs
 """
+
 
 def insertion_sort(ar):
     for i in range(1,len(ar)):
@@ -13,7 +16,9 @@ def insertion_sort(ar):
             ar[j+1]=ar[j]
             j-=1
         ar[j+1]=value
-        
+
+
+
 """
 this is not a merge sort.
 it only merges but expects arguments
@@ -30,11 +35,36 @@ def merge(ar, run):
     return [run[0]] + merge(ar+run[1:])
     
 def tim_sort(ar):
-    print("todo")
+    runs = []
+    arun = [ar[0]]
+    i = 1
+    while i < len(ar):
+        if ar[i] < ar[i-1]:
+            runs.append(arun)
+            arun = [ar[i]]
+        else:
+            arun.append(ar[i])
+            
+        i+=1
+    runs.append(arun)
+    
+    sortedRuns = []
+    sortedAr = []
+    for run in runs:
+        sortedRuns.append(insertion_sort(run))
+    for run in sortedRuns:
+        sortedAr = merge(sortedAr,run)
+    return sortedAr
+
+    
     
 ar=[43,55,2,6,7,65,3,2,8,77,78,4,39,40,41,1,67]
 
 #insertion_sort(ar)
-tim_sort(ar)
+ar = tim_sort(ar)
 print(ar)
+
+
+
+
 
