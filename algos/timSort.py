4ece6eb33bfbd89e530b47009e217513e836d934
paul-brisson <brissonpauljr@gmail.com>
2022-09-11 19:09:16 +0200

    Update timSort.py

--- algos/timSort.py
+++ algos/timSort.py
@@ -14,9 +14,20 @@ def insertion_sort(ar):
             j-=1
         ar[j+1]=value
         
-    
-def merge_sort(ar, run):
-    print("todo")
+"""
+this is not a merge sort.
+it only merges but expects arguments
+are already sorted asc
+"""
+def merge(ar, run):
+    if not ar:
+        return run
+    if not run:
+        return ar
+
+    if ar[0]<=run[0]:
+        return [ar[0]] + merge(ar[1:],run)
+    return [run[0]] + merge(ar+run[1:])
     
 def tim_sort(ar):
     print("todo")
@@ -26,3 +37,4 @@ ar=[43,55,2,6,7,65,3,2,8,77,78,4,39,40,41,1,67]
 #insertion_sort(ar)
 tim_sort(ar)
 print(ar)
+
